fb2940131f538ac7b7b7ec8f5ea5f296c5383400
serendwin <twentyhours133@gmail.com>
2025-12-10 01:28:29 +0700

    bikin beranda sama nambahin detail berita

--- scripts/pages.js
+++ scripts/pages.js
@@ -1,8 +1,120 @@
 const pages = {
     beranda: `
-        <h2 class="text-xl font-bold mb-2">Beranda</h2>
-        <p>Selamat datang di BeritaKita.</p>
+    <!-- HERO -->
+    <section class="max-w-15xl mx-auto px-0 mt-0">
+        <div class="bg-white shadow-sm rounded-2xl overflow-hidden transition transform duration-300 hover:scale-[1.02] active:scale-[0.95] cursor-pointer">
+            <img src="https://i.imgur.com/VZbj9h9.jpeg" class="w-full h-72 object-cover" />
+
+            <div class="p-8">
+                <h1 class="text-3xl font-bold mb-2">Berita Utama: Perkembangan AI Terbaru</h1>
+                <p class="text-gray-600 w-3/4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="max-w-10xl mx-auto px-0 mt-5">
+        <h2 class="text-2xl font-bold mb-4">Berita Terbaru</h2>
+        <div class="grid grid-cols-3 gap-10">
+
+            <div class="bg-white rounded-xl shadow-sm overflow-hidden transform duration-300 hover:scale-[1.02] active:scale-[0.95] cursor-pointer">
+                <img src="https://i.imgur.com/7Z6P1Gk.jpeg" class="w-full h-40 object-cover"/>
+                <div class="p-4">
+                    <h3 class="font-semibold">Berita Utama: Perkembangan AI Terbaru</h3>
+                    <p class="text-gray-600 text-sm mt-1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                    </p>
+                </div>
+            </div>
+
+            <div class="bg-white rounded-xl shadow-md overflow-hidden transform duration-300 hover:scale-[1.02] active:scale-[0.95] cursor-pointer">
+                <img src="https://i.imgur.com/khvXTiX.jpeg" class="w-full h-40 object-cover"/>
+                <div class="p-4">
+                    <h3 class="font-semibold">Berita Utama: Perkembangan AI</h3>
+                    <p class="text-gray-600 text-sm mt-1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                    </p>
+                </div>
+            </div>
+
+            <div class="bg-white rounded-xl shadow-md overflow-hidden transform duration-300 hover:scale-[1.02] active:scale-[0.95] cursor-pointer">
+                <img src="https://i.imgur.com/2hCIHqW.jpeg" class="w-full h-40 object-cover"/>
+                <div class="p-4">
+                    <h3 class="font-semibold">Berita Soft: Contoh Olahraga</h3>
+                    <p class="text-gray-600 text-sm mt-1">
+                        Sensai lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+            </div>
+
+            <div class="bg-white rounded-xl shadow-md overflow-hidden transform duration-300 hover:scale-[1.02] active:scale-[0.95] cursor-pointer">
+                <img src="https://i.imgur.com/2hCIHqW.jpeg" class="w-full h-40 object-cover"/>
+                <div class="p-4">
+                    <h3 class="font-semibold">Berita Soft: Contoh Olahraga</h3>
+                    <p class="text-gray-600 text-sm mt-1">
+                        Sensai lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+            </div>
+            
+            <div class="bg-white rounded-xl shadow-md overflow-hidden transform duration-300 hover:scale-[1.02] active:scale-[0.95] cursor-pointer">
+                <img src="https://i.imgur.com/2hCIHqW.jpeg" class="w-full h-40 object-cover"/>
+                <div class="p-4">
+                    <h3 class="font-semibold">Berita Soft: Contoh Olahraga</h3>
+                    <p class="text-gray-600 text-sm mt-1">
+                        Sensai lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+            </div>
+
+            <div class="bg-white rounded-xl shadow-md overflow-hidden transform duration-300 hover:scale-[1.02] active:scale-[0.95] cursor-pointer">
+                <img src="https://i.imgur.com/2hCIHqW.jpeg" class="w-full h-40 object-cover"/>
+                <div class="p-4">
+                    <h3 class="font-semibold">Berita Soft: Contoh Olahraga</h3>
+                    <p class="text-gray-600 text-sm mt-1">
+                        Sensai lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="max-w-10xl mx-auto px-0 mt-5">
+        <h2 class="text-2xl font-bold mb-6">KATEGORI</h2>
+
+        <div class="grid grid-cols-5 gap-6">
+
+            <div class="bg-white p-6 rounded-xl shadow-sm flex flex-col justify-center items-center gap-2">
+                <span class="text-3xl">🏛</span>
+                <p class="font-medium">Politik</p>
+            </div>
+
+            <div class="bg-white p-6 rounded-xl shadow-sm flex flex-col justify-center items-center gap-2">
+                <span class="text-3xl">💻</span>
+                <p class="font-medium">Teknologi</p>
+            </div>
+
+            <div class="bg-white p-6 rounded-xl shadow-sm flex flex-col justify-center items-center gap-2">
+                <span class="text-3xl">⚽</span>
+                <p class="font-medium">Olahraga</p>
+            </div>
+
+            <div class="bg-white p-6 rounded-xl shadow-sm flex flex-col justify-center items-center gap-2">
+                <span class="text-3xl">🎬</span>
+                <p class="font-medium">Hiburan</p>
+            </div>
+
+            <div class="bg-white p-6 rounded-xl shadow-sm flex flex-col justify-center items-center gap-2">
+                <span class="text-3xl">⋯</span>
+                <p class="font-medium">Lainnya</p>
+            </div>
+
+        </div>
+    </section>
     `,
+
     kategori: `
         <div class="flex flex-col gap-8">
             <div class="flex flex-col gap-4">
